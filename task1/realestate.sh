f98fb6c9d15f1c4c7249f230a4143e90899a58e3
devansh-dvj <devansh.dvj@gmail.com>
2020-09-20 11:43:34 +0530

    Task 1 : Fixed the issue, working

--- task1/realestate.sh
+++ task1/realestate.sh
@@ -16,17 +16,17 @@ file3="$3"
 awk -v q="'" -F ',' '
 BEGIN {}
 {profit=0}
-{rate=$4}
+{base_rate=$4}
 {maintainance=$6}
 {area=0.9*$2}
 {
     for (i = 0; i < $3; i++) {
-        profit += (area*rate - maintainance);
-        rate += (rate*$5) / 100;
+        profit += 12*int(area*base_rate - maintainance);
+        base_rate += int((base_rate*$5) / 100);
     }
 }
 {print $1 "," profit "," $3}
-# {print profit}
-' ${csv_file}
+' ${csv_file} | sort --field-separator="," -n -k3 | sort --field-separator="," -n -rk2,2 >${file2}
 
-sort -n -k 2 x >y
+awk -v q="'" -F ',' '{print $1}' ${file2} >${file3}
+# cat ${file2} ${file3}
