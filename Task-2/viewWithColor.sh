68510df0687423c4fbcbb9be4a85e269e5ecc81c
devansh-dvj <devansh.dvj@gmail.com>
2020-09-26 16:05:27 +0530

    Tested on docker

--- Task-2/viewWithColor.sh
+++ Task-2/viewWithColor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-RAW=$1 # The raw, uncoloured input
+# Task 2 B
+
+RAW=$1   # The raw, uncoloured input
 GUIDE=$2 # The color guide
 
 awk -v RAW=$RAW '
@@ -16,14 +18,14 @@ BEGIN {
         font = $3"_FONT"
         bg   = $4"_BACKGROUND"
         # Append font colour and BG color to the start of the line containing the fields
-        printf("-e \"/"$1"/ s/^/\"${"bg"}${"font"}\"/\" ") 
+        printf("-e \"/"$1"/ s/^/\"${"bg"}${"font"}\"/\" ")
     }
 }
 END {
     # Reset all colours before printing
-    printf("-e \"4,$ s/$/\"${RESET_ALL}\"/ p\" -e \"1,3 p\" "RAW) 
+    printf("-e \"4,$ s/$/\"${RESET_ALL}\"/ p\" -e \"1,3 p\" "RAW)
 }
-' $GUIDE > task_2_temp_file_1 # Redirect to temporary file "y"
+' $GUIDE >task_2_temp_file_1 # Redirect to temporary file "y"
 
 bash task_2_temp_file_1 # This executes the sed commands inside "y"
 
