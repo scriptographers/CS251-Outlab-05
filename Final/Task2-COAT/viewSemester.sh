e55bf2c8810bfacf84dc9433d93a873ddad86422
devansh-dvj <devansh.dvj@gmail.com>
2020-09-29 11:06:29 +0530

    Fixed issue in Task-2C,2D

--- Final/Task2-COAT/viewSemester.sh
+++ Final/Task2-COAT/viewSemester.sh
@@ -7,7 +7,28 @@ file="$1"
 season="$2"
 year="$3"
 
-# sed -n -r "/$season/{/$year/ p}" $file
+# sed -n -r "/$course_code/ p" $file
 sed -n 1,3p $file # Print the header
+
+# Check whether with or without color
+s=$(awk '
+BEGIN {
+    FS = " "
+    RS = "\n"
+}
+END {
+    if (NF==8) {
+        print 0 # without color
+    }
+    else {
+        print 1 # with color
+    }
+}
+' $file)
+
 # As mentioned in the PS that there are no “Autumn” or “Spring” as a substring in the name of any course neither does the year match with any course code
-sed -n -r "/$season/{/$year/ p}" $file | sort -k3 # Sort according to course code
+if [[ $s == 1 ]]; then
+    sed -n -r "/$season/{/$year/ p}" $file | sort -k4 # Sort according to course code
+else
+    sed -n -r "/$season/{/$year/ p}" $file | sort -k3 # Sort according to course code
+fi
--- Task-2/viewCourse.sh
+++ Task-2/viewCourse.sh
@@ -8,5 +8,26 @@ course_code="$2"
 
 # sed -n -r "/$course_code/ p" $file
 sed -n 1,3p $file # Print the header
+
+# Check whether with or without color
+s=$(awk '
+BEGIN {
+    FS = " "
+    RS = "\n"
+}
+END {
+    if (NF==8) {
+        print 0 # without color
+    }
+    else {
+        print 1 # with color
+    }
+}
+' $file)
+
 # According to the update, the query can only occur as a substring of the course codes
-sed -n -r "/$course_code/ p" $file | sort -k3 # Sort according to course code
+if [[ $s == 1 ]]; then
+    sed -n -r "/$course_code/ p" $file | sort -k4 # Sort according to course code
+else
+    sed -n -r "/$course_code/ p" $file | sort -k3 # Sort according to course code
+fi
